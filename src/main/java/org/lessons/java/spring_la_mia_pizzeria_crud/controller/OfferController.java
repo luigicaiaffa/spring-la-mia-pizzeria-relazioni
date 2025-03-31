9cf17127fad0c0242f15e69ac7220c5e09f11d5f
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-31 15:56:23 +0200

    fix fragments

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/OfferController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/OfferController.java
@@ -1,7 +1,6 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Offer;
-import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.OfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
