144ad82529e52804249ee3ec93cc5fbb99b5f790
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-04-01 16:15:13 +0200

    Add index

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -0,0 +1,24 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/ingredients")
+public class IngredientController {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @GetMapping
+    public String index(Model model) {
+
+        model.addAttribute("ingredients", ingredientRepository.findAll());
+        return "/ingredients/index";
+    }
+
+}
