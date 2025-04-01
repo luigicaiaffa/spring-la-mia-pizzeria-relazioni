212379a62dbead0511959d53d445efa3e86ed22a
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-04-01 16:45:07 +0200

    Add create ingredient

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -1,11 +1,18 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Ingredient;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -19,6 +26,33 @@ public class IngredientController {
 
         model.addAttribute("ingredients", ingredientRepository.findAll());
         return "/ingredients/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+
+        model.addAttribute("ingredient", new Ingredient());
+        return "/ingredients/create-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "/ingredients/create-edit";
+        } else {
+            ingredientRepository.save(formIngredient);
+            return "redirect:/ingredients";
+        }
+    }
+
+    @GetMapping("/edit/{id}")
+    public String getMethodName(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("edit", true);
+        model.addAttribute("ingredient", ingredientRepository.findById(id).get());
+        return "/ingredients/create-edit";
     }
 
 }
